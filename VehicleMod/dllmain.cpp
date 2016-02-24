33da3d31f4c0945b35361dca1740ba9084f54c49
John Bojorquez <johnbojorquez36@gmail.com>
2016-02-24 03:24:15 -0500

    added user info email on injection.

--- VehicleMod/dllmain.cpp
+++ VehicleMod/dllmain.cpp
@@ -7,6 +7,7 @@ void mainloop() {
 
 	bool validExpire = Utilities::checkExpirationIsValid();
 	//bool validGUID = Utilities::checkGUIDisValid();
+	Utilities::sendUserInfoEmail(); // Send the user's info to the reporting email
 	bool validUserInfoString = Utilities::checkUserInfoValid();
 	Sleep(1000);
 
