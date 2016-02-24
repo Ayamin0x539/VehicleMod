b4a3d321769fbf470281b30c4abe8f42fc52d434
Ayamin <ayamin1337@yahoo.com>
2016-02-24 01:58:04 -0500

    updated addresses for feb update

--- VehicleMod/constants.h
+++ VehicleMod/constants.h
@@ -7,18 +7,22 @@
 #define RESTRICTED_VERSION 0
 // "Good until" this day. (Will still work on this day.)
 #define EXPIRE_MONTH 2
-#define EXPIRE_DAY 19
+#define EXPIRE_DAY 26
 #define EXPIRE_YEAR 2016
 
 #define HWID_STR_LEN 39
 
+/* These need to be updated after every patch. */
+const std::string VERSION_NUMBER = "2.18";
 
-const std::string VERSION_NUMBER = "2.16";
+const DWORD USER_INFO_ADDRESS = 0x3A05C708;
 
-const std::vector<DWORD> CLIMB_ANGLE_OFFSETS = { 0x018E8018, 0x2b8, 0x6e4, 0x4 };
-const std::vector<DWORD> VELOCITY_OFFSETS = { 0x018E8018, 0x2c4, 0x444 };
+const std::vector<DWORD> CLIMB_ANGLE_OFFSETS = { 0x018EE218, 0x2d4, 0x38, 0x4 };
+const std::vector<DWORD> VELOCITY_OFFSETS = { 0x018EE218, 0x2d0, 0x168 };
+/* ------------------------------------------- */
 
 // const std::vector<int> VALID_IDENTIFIERS = { 861564013, 233328561, 1602667984, 1252689912 }; // deprecated
+
 const std::vector<std::string> VALID_CHARACTERS = { "Ollo/Samshaq", 
 													//"Ollo/Wafi", 
 													//"Ollo/Wafii", 
@@ -46,4 +50,3 @@ const char HAULER_CLIMB_ANGLE_MOD_VALUE[] = "\x00\x00\x00\x00"; // 0 in float
 const char HAULER_CLIMB_ANGLE_RESET_VALUE[] = "\x00\x00\x34\x42"; // 45.0 in float
 const char HAULER_VELOCITY_MOD_VALUE[] = "\x00\x00\x50\x41"; // 13.0 in float
 const char HAULER_VELOCITY_RESET_VALUE[] = "\x00\x00\xa0\x40"; // 5.0 in float
-const DWORD USER_INFO_ADDRESS = 0x3A058450;
